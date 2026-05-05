75c95f6560cc271c5c6d5d805b5508a5ec0f4a1a
Caleb Lochner <lochner@uiwtx.edu>
2026-05-05 17:24:13 -0500

    I dont even know

--- main.py
+++ main.py
@@ -49,16 +49,35 @@ def display():
     bfgFrame30 = ttk.Frame(baseFrame, relief = 'sunken', width = 30, height = 30)
     bfgFrame40 = ttk.Frame(baseFrame, width = 80, height = 30)
 
-    bfgFrame01 = ttk.Frame(baseFrame, relief = 'sunken', width = 460, height = 230)
+    bfgFrame01 = ttk.Frame(baseFrame, relief = 'sunken', width = 460, height = 225)
+
+    Frame01g00 = ttk.Frame(bfgFrame01, relief = 'raised', width = 60, height = 31)
+    Frame01g10 = ttk.Frame(bfgFrame01, relief = 'raised', width = 370, height = 19)
+    Frame01g11 = ttk.Frame(bfgFrame01, relief = 'raised', width = 370, height = 19)
+    Frame01g20 = ttk.Frame(bfgFrame01, relief = 'raised', width = 72, height = 19)
+    Frame01g21 = ttk.Frame(bfgFrame01, relief = 'raised', width = 65, height = 19)
+    Frame01g22 = ttk.Frame(bfgFrame01, relief = 'raised', width = 65, height = 19)
+    Frame01g32 = ttk.Frame(bfgFrame01, relief = 'raised', width = 65, height = 19)
+
+    Frame01g01 = ttk.Frame(bfgFrame01, relief = 'raised', width = 60, height = 31)
+    Frame01g02 = ttk.Frame(bfgFrame01, relief = 'raised', width = 90, height = 185)
+    Frame01g12 = ttk.Frame(bfgFrame01, relief = 'raised', width = 370, height = 185)
 
     #Widgets
         #Labels
     cloud_label0 = ttk.Label(bfgFrame10, image = cloudPNGtk)
     cloud_label1 = ttk.Label(bfgFrame30, image = cloudPNGtk)
     bigTitle = ttk.Label(bfgFrame20, text = 'Weather XML Grabber', font = 'tFont.TLabel', width = 20, anchor = CENTER)
+    xmlSiteLabel = ttk.Label(Frame01g00, text = 'XML Site:', width = 14)
+    destLabel = ttk.Label(Frame01g01, text = 'Destination File:')
+
         #Buttons
+    saveB = ttk.Button(Frame01g21, text = 'Save As')
+    updateB = ttk.Button(Frame01g02, text = 'Update')
 
         #Entry Fields
+    xmlSiteField = ttk.Entry(Frame01g10, width = 48)
+    xmlDestField = ttk.Entry(Frame01g11, width = 48)
 
     #Place Frames
     baseFrame.grid(column = 0, row = 0, sticky = (N, S, W, E))
@@ -72,14 +91,32 @@ def display():
 
     bfgFrame01.grid(column = 0, row = 1, columnspan = 5)
 
+    Frame01g00.grid(column = 0, row = 0)
+    Frame01g10.grid(column = 1, row = 0)
+    Frame01g11.grid(column = 1, row = 1)
+    Frame01g20.grid(column = 2, row = 0)
+    Frame01g21.grid(column = 2, row = 1)
+    Frame01g22.grid(column = 2, row = 2)
+    Frame01g33.grid(column = 3, row = 3)
+
+    Frame01g01.grid(column = 0, row = 1)
+    Frame01g02.grid(column = 0, row = 2)
+    Frame01g12.grid(column = 1, row = 2, columnspan = 2)
+
     #Place Widgets
         #Place Labels
     cloud_label0.grid(column = 0, row = 0)
     cloud_label1.grid(column = 0, row = 0)
     bigTitle.grid(column = 0, row = 0, sticky = (N))
+    xmlSiteLabel.grid(column = 0, row = 0, sticky = (N, W))
+    destLabel.grid(column = 0, row = 0, sticky = (N, W))
         #Place Buttons
+    saveB.grid(column = 0, row = 0)
+    updateB.grid(column = 0, row = 0, columnspan = 2)
 
         #Place Entry Fields
+    xmlSiteField.grid(column = 0, row = 0, sticky = (W))
+    xmlDestField.grid(column = 0, row = 0, sticky = (W))
 
     #Main loop for display
     root.mainloop()
