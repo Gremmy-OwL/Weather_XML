0a036e674bfec2ef486c1a4de3031cb06607eb98
Caleb Lochner <lochner@uiwtx.edu>
2026-05-05 14:54:13 -0500

    GUI Grid Base Layout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 ''''
 Author: Gremmy OwL
-Version: 0.0.3 B
+Version: 0.0.4 B
 Description: Pull XML files
 '''
 #Imports
@@ -29,6 +29,52 @@ def display():
     root.title('Weather XML Grabber')   #Set window title
     root.geometry('480x270')
     root.resizable(False, False)    
+
+    #Styles
+    '''For later use'''
+
+    #Tk Variables
+    cloudpil = Image.open(cloudpng)
+    cloud_resize = cloudpil.resize((30, 30))
+    cloudPNGtk = ImageTk.PhotoImage(cloud_resize)
+
+    #Frames
+    baseFrame = ttk.Frame(root, padding = (10, 5, 10, 5), relief = 'raised', width = 480, height = 270)
+
+    #Grid Frames in baseFrame
+    bfgFrame00 = ttk.Frame(baseFrame, relief = 'sunken', width = 85, height = 30)
+    bfgFrame10 = ttk.Frame(baseFrame, relief = 'sunken', width = 30, height = 30)
+    bfgFrame20 = ttk.Frame(baseFrame, relief = 'sunken', width = 230, height = 30)
+    bfgFrame30 = ttk.Frame(baseFrame, relief = 'sunken', width = 30, height = 30)
+    bfgFrame40 = ttk.Frame(baseFrame, relief = 'sunken', width = 85, height = 30)
+
+    bfgFrame01 = ttk.Frame(baseFrame, relief = 'sunken', width = 460, height = 230)
+
+    #Widgets
+        #Labels
+
+        #Buttons
+
+        #Entry Fields
+
+    #Place Frames
+    baseFrame.grid(column = 0, row = 0, sticky = (N, S, W, E))
+
+    #bfgFrames
+    bfgFrame00.grid(column = 0, row = 0)
+    bfgFrame10.grid(column = 1, row = 0)
+    bfgFrame20.grid(column = 2, row = 0)
+    bfgFrame30.grid(column = 3, row = 0)
+    bfgFrame40.grid(column = 4, row = 0)
+
+    bfgFrame01.grid(column = 0, row = 1, columnspan = 5)
+
+    #Place Widgets
+        #Place Labels
+
+        #Place Buttons
+
+        #Place Entry Fields
 
     #Main loop for display
     root.mainloop()
