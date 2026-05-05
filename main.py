03cc756c6be793192ce77a33f83b448d92dd447d
Caleb Lochner <lochner@uiwtx.edu>
2026-05-05 14:21:24 -0500

    Display def

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 ''''
 Author: Gremmy OwL
-Version: 0.0.2 B
+Version: 0.0.3 B
 Description: Pull XML files
 '''
 #Imports
@@ -17,3 +17,29 @@ import requests, os
 save_path = 'weather.xml'
 cloudpng = os.path.normpath('Assets\Images\cloud.png')
 cloudico = os.path.normpath('Assets\Images\cloud.ico')
+
+#Functions
+
+#GUI Function
+def display():
+
+    #Tkinter window
+    root = Tk()
+    root.iconbitmap(cloudico)   #Set icon as cloud icon
+    root.title('Weather XML Grabber')   #Set window title
+    root.geometry('480x270')
+    root.resizable(False, False)    
+
+    #Main loop for display
+    root.mainloop()
+    return 0
+#Main function
+def main():
+
+    #Calls display to run GUI
+    display()
+
+    return 0
+
+#Calls
+main()
