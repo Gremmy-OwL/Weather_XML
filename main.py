f643215398bd70f63c3efd3c089ffbd281415557
Caleb Lochner <lochner@uiwtx.edu>
2026-05-05 13:55:04 -0500

    Imports added

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 ''''
 Author: Gremmy OwL
-Version: 0.0.0B
+Version: 0.0.1 B
 Description: Pull XML files
 '''
+#Imports
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
+from tkinter import font as tkfont
+from PIL import Image
+from PIL import ImageTk
+from time import sleep
+import requests, os
